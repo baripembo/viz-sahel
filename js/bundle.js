678d911f187547ea15ff7e7654d28ab1281ac0c6
baripembo <erikawei@gmail.com>
2021-08-18 18:26:57 -0400

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -101,6 +101,29 @@ $( document ).ready(function() {
 	});
 });
 
+$( document ).ready(function() {
+	// d3.csv('../data/cadre_harmonise.csv', function(data) {
+	// 	let array = new Array();
+	// 	array.push(data);
+	// 	//let disasterByDecadeType = d3.group(array, d => d.decade, d => d.disaster_type);
+	// 	console.log(data);
+	// });
+
+
+
+      d3.csv('../data/cadre_harmonise.csv').then(function(data) {
+      console.log('Data loaded', data);
+      let group = d3.group(data, d => d.exercise_label + ' ' + d.exercise_year, d => d.adm0_name);
+
+      // let dataByCountry = d3.nest()
+      //   .key(function(d) { return d.adm0_name; })
+      //   .object(data[0]);
+
+
+      console.log(group)
+    }
+  );
+});
 /*! jQuery UI - v1.12.1 - 2021-07-28
 * http://jqueryui.com
 * Includes: effect.js
@@ -1788,8 +1811,6 @@ $( document ).ready(function() {
         left: '100vw'
       }, 500, 'easeOutQuart');
     });
-
-    initSections();
   }
 
   function preload(imgArray) {
@@ -1807,10 +1828,14 @@ $( document ).ready(function() {
   }
 
   function loadComplete() {
+    initDial();
+    initSections();
+
     $('.loader').hide();
     $('main').css('opacity', 1);
+  }
 
-    //init dial
+  function initDial() {
     dialHeight = $('.dial').height();
     dialWidth = viewportWidth>=768 ? 800 : viewportWidth*1.2;
   }
