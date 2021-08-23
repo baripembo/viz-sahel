e2e0b9a3fd0b59ed57fcd0ac4dfcfed7740025e3
baripembo <erikawei@gmail.com>
2021-08-23 16:34:27 -0400

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -2058,7 +2058,7 @@ $( document ).ready(function() {
       'page title': document.title,
       'embedded in': window.location.href,
       'action': 'switch viz',
-      'viz type': 'oad covid-19',
+      'viz type': 'climate change data story',
       'current view': view,
       'content': content
     });
