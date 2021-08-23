82ec03abfedcf0f398b64e2934c383f4f6c3f333
baripembo <erikawei@gmail.com>
2021-08-23 16:33:31 -0400

    add mixpanel events

--- src/js/viz.js
+++ src/js/viz.js
@@ -2,8 +2,10 @@ $( document ).ready(function() {
   let viewportWidth = $(window).width()
   let currentSection = 1;
   let dialHeight, dialWidth;
+  let map;
   const rotations = [0, 0, 0, 45, 15, -15, -45];
-  const credits = ['©UNICEF/UNI82205/Holt','Photo credit','©UNICEF Chad/2016/Bahaji','Photo credit','©UNICEF Chad/2016/Bahaji'];
+  const credits = ['©UNICEF/UNI82205/Holt','','©UNICEF Chad/2016/Bahaji','Photo credit','©UNICEF Chad/2016/Bahaji'];
+
 
   function init() {
     //preload images
@@ -34,6 +36,24 @@ $( document ).ready(function() {
       $('.dataviz-container').animate({
         left: '100vw'
       }, 500, 'easeOutQuart');
+    });
+
+    //track links
+    $('.linkTrack').on('click', function() {
+      let url = $(this).attr('href');
+      console.log(url)
+      mpTrackLink(url);
+    });
+
+    //init mapbox
+    mapboxgl.accessToken = 'pk.eyJ1IjoiaHVtZGF0YSIsImEiOiJja2FvMW1wbDIwMzE2MnFwMW9teHQxOXhpIn0.Uri8IURftz3Jv5It51ISAA';
+    map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/humdata/ckfx2jgjd10qx1bnzkla9px41/',
+      center: [15.4723, 12],
+      minZoom: 2,
+      zoom: 3.5,
+      attributionControl: false
     });
   }
 
@@ -77,23 +97,51 @@ $( document ).ready(function() {
         var id = Number($(e.target.triggerElement()).data('section'));
         currentSection = id;
         setSection(currentSection-1, 0);
+        setPhotoCredit(currentSection-1);
         setDial(currentSection, 'enter');
       })
       .on('leave', function(e) {
         var id = Number($(e.target.triggerElement()).data('section'));
         currentSection = id-1;
         setSection(currentSection, 1);
+        setPhotoCredit(currentSection-1);
         setDial(currentSection, 'leave');
       })
       //.addIndicators()
       .addTo(controller);
     }
+
+    new ScrollMagic.Scene({
+      triggerElement: document.querySelector('#mapZoomTrigger'),
+      triggerHook: 0
+    })
+    .on('enter', function(e) {
+      var location = {
+        center: [15.4723, 12],
+        zoom: 6,
+        pitch: 100,
+        bearing: 0
+      };
+      map.flyTo(location);
+    })
+    .on('leave', function(e) {
+      var location = {
+        center: [15.4723, 12],
+        zoom: 3.5,
+        pitch: 0,
+        bearing: 0
+      };
+      map.flyTo(location);
+    })
+    //.addIndicators()
+    .addTo(controller);
   }
 
   function setSection(step, opacity) {
     $('[data-item="'+step+'"]').css('opacity', opacity);
+  }
 
-    //set photo credit
+  function setPhotoCredit(step) {
     $('.credit').html(credits[step]);
     if (step==6)
       $('.credit').hide();
@@ -128,11 +176,13 @@ $( document ).ready(function() {
   function showDataviz() {
     $('.dataviz-container').find('.dataviz').hide();
     $('.dataviz-container').find('.dataviz-'+currentSection).show();
+
+    mpTrackInteraction('dataviz view', currentSection);
   }
 
   function initTracking() {
     //initialize mixpanel
-    let MIXPANEL_TOKEN = '';
+    let MIXPANEL_TOKEN = window.location.hostname==='data.humdata.org'? '5cbf12bc9984628fb2c55a49daf32e74' : '99035923ee0a67880e6c05ab92b6cbc0';
     mixpanel.init(MIXPANEL_TOKEN);
     mixpanel.track('page view', {
       'page title': document.title,
@@ -140,6 +190,26 @@ $( document ).ready(function() {
     });
   }
 
-  //initTracking();
+  function mpTrackInteraction(view, content) {
+    mixpanel.track('viz interaction', {
+      'page title': document.title,
+      'embedded in': window.location.href,
+      'action': 'switch viz',
+      'viz type': 'oad covid-19',
+      'current view': view,
+      'content': content
+    });
+  }
+
+  function mpTrackLink(url) {
+    mixpanel.track('link click', {
+      'embedded in': window.location.href,
+      'destination url': url,
+      'link type': 'link click',
+      'page title': document.title
+    });
+  }
+
+  initTracking();
   init();
 });
